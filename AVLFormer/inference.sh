f5db204055a882eb1505bec5c7ff3afa29d51ce6
Xuyang Shen <xuyangshen@outlook.com>
2023-11-23 05:38:59 +0000

    Fix inference typo

--- AVLFormer/inference.sh
+++ AVLFormer/inference.sh
@@ -1,6 +1,7 @@
-python src/tasks/inference.py \ 
+python \
+    src/tasks/inference.py \
     --eval_model_dir output/favd_default \
     --test_video_fname datasets/videos/test \
-    --test_audio_fname datasets/mp3_audio/test \
+    --test_audio_fname datasets/audio_mp3/test \
     --do_lower_case \
     --do_test \
